a2d34e65d701cf0a30a96918ddee76deb39c9669
jslusser <jslusser@gmail.com>
2017-09-29 14:50:24 -0400

    Thu night changes

--- BfastBowlBldr/AddInTableViewController.swift
+++ BfastBowlBldr/AddInTableViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class AddInTableViewController: UITableViewController {
     
+    var ingredients = [Ingredient]()
+    var selectedIngredients = [Ingredient]()
+    
     var addinImages = [String]()
     var addinNames = [String]()
     var addinCopy = [String]()
     var addinInfo = [String]()
     var addinPurch = [String]()
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -90,82 +93,82 @@ class AddInTableViewController: UITableViewController {
         
         
         tableView.estimatedRowHeight = 50
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return addinNames.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "AddInTableCell", for: indexPath) as! AddInTableViewCell
-     
-     let row = indexPath.row
-     cell.addinName.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
-     cell.addinName.text = addinNames[row]
-     cell.addinImage.image = UIImage(named: addinImages[row])
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddInTableCell", for: indexPath) as! AddInTableViewCell
+        
+        let row = indexPath.row
+        cell.addinName.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        cell.addinName.text = addinNames[row]
+        cell.addinImage.image = UIImage(named: addinImages[row])
+        
         // Configure the cell...
-
+        
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-  
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -182,6 +185,7 @@ class AddInTableViewController: UITableViewController {
             detailViewController.ingredientPurchase = addinPurch[row]
         }
     }
- 
-
+    
+    
 }
+
--- BfastBowlBldr/IngredientTableViewCell.swift
+++ BfastBowlBldr/IngredientTableViewCell.swift
@@ -10,14 +10,14 @@ import UIKit
 
 protocol CellProtocol : class {
     func switchButtonTapped(WithStatus status : Bool , ForCell myCell : IngredientTableViewCell)
-    }
+}
 
 
 
 class IngredientTableViewCell: UITableViewCell {
     
     weak var delegate : CellProtocol?
-   
+    
     @IBOutlet weak var ingredientImage: UIImageView!
     
     @IBOutlet weak var ingredientName: UILabel!
@@ -29,37 +29,38 @@ class IngredientTableViewCell: UITableViewCell {
         self.delegate?.switchButtonTapped(WithStatus: sender.isOn, ForCell: self)
     }
     
-//        if ingredientSwitch.isOn {
-//            onCall()
-//            // print("Switch is on")
-//            ingredientSwitch.setOn(true, animated: true)
-//        } else {
-//            offCall()
-//            //print("Switch is off")
-//            ingredientSwitch.setOn(false, animated: true)
-//       }
-//    }
-//
-//    func onCall() {
-//       print("Switch is on")
-//    }
-//
-//    func offCall() {
-//        print("Switch is off")
-//    }
-
+    //        if ingredientSwitch.isOn {
+    //            onCall()
+    //            // print("Switch is on")
+    //            ingredientSwitch.setOn(true, animated: true)
+    //        } else {
+    //            offCall()
+    //            //print("Switch is off")
+    //            ingredientSwitch.setOn(false, animated: true)
+    //       }
+    //    }
+    //
+    //    func onCall() {
+    //       print("Switch is on")
+    //    }
+    //
+    //    func offCall() {
+    //        print("Switch is off")
+    //    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         // Initialization code
     }
-// the following is left over from the stackoverflow solution - I think its safe to delete
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//        self.delegate?.switchButtonTapped(WithStatus: selected, ForCell: self)
-
-        // Configure the view for the selected state
- //   }
-
+    // the following is left over from the stackoverflow solution - I think its safe to delete
+    //    override func setSelected(_ selected: Bool, animated: Bool) {
+    //        super.setSelected(selected, animated: animated)
+    //        self.delegate?.switchButtonTapped(WithStatus: selected, ForCell: self)
+    
+    // Configure the view for the selected state
+    //   }
+    
 }
+
